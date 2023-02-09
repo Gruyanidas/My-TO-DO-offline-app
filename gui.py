063fb33061594efef52e499c9f664a5ef9b06091
Milos <milosgrujic1987@gmail.com>
2023-02-09 16:00:23 +0100

    Ubaceno EDIT dugme

--- gui.py
+++ gui.py
@@ -4,9 +4,12 @@ import PySimpleGUI as sg
 label = sg.Text(" Type in a to-do: ")  # tekst label na window, mora da bude string
 input_box = sg.InputText(tooltip="Enter to-do", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functionsFromTO_DO.get_to_dos(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))  # layout ocekuje litu, kada su u jednoj listi zajedno onda su istom redu
 while True:
     event, values = window.read()
@@ -18,6 +21,20 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functionsFromTO_DO.write_to_dos(todos)
+            window['todos'].update(todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            to_dos = functionsFromTO_DO.get_to_dos()
+            index = to_dos.index(todo_to_edit)
+            to_dos[index] = new_todo
+
+            functionsFromTO_DO.write_to_dos(to_dos)
+            window['todos'].update(values=to_dos)
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 window.close()
