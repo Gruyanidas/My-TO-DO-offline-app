64202faf1e33bec5b3cb745bd2c68aa8a5aae430
Milos <milosgrujic1987@gmail.com>
2023-02-09 20:42:35 +0100

    Ubaceno complete i exit dugme

--- gui.py
+++ gui.py
@@ -7,9 +7,12 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functionsFromTO_DO.get_to_dos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label], [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica', 20))  # layout ocekuje litu, kada su u jednoj listi zajedno onda su istom redu
 while True:
     event, values = window.read()
@@ -22,7 +25,7 @@ while True:
             todos.append(new_todo)
             functionsFromTO_DO.write_to_dos(todos)
             window['todos'].update(todos)
-
+            window['todo'].update(value='')
         case "Edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
@@ -33,6 +36,15 @@ while True:
 
             functionsFromTO_DO.write_to_dos(to_dos)
             window['todos'].update(values=to_dos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functionsFromTO_DO.get_to_dos()
+            todos.remove(todo_to_complete)
+            functionsFromTO_DO.write_to_dos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case "todos":
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
