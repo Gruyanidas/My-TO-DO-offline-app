9017e7730d58551e470a28ba967c21aa36b90f11
Milos <milosgrujic1987@gmail.com>
2023-02-09 14:22:10 +0100

    Ubaceno ADD dugme

--- gui.py
+++ gui.py
@@ -2,9 +2,22 @@ from moduls import functionsFromTO_DO
 import PySimpleGUI as sg
 
 label = sg.Text(" Type in a to-do: ")  # tekst label na window, mora da bude string
-input_box = sg.InputText(tooltip="Enter to-do")
+input_box = sg.InputText(tooltip="Enter to-do", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My To-Do App", layout=[[label], [input_box, add_button]])  # layout ocekuje litu, kada su u jednoj listi zajedno onda su istom redu u app
-window.read()  # display a window on the screen
+window = sg.Window("My To-Do App",
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))  # layout ocekuje litu, kada su u jednoj listi zajedno onda su istom redu
+while True:
+    event, values = window.read()
+    print(event)  # ADD
+    print(values)  # recnik todo:input
+    match event:
+        case "Add":
+            todos = functionsFromTO_DO.get_to_dos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functionsFromTO_DO.write_to_dos(todos)
+        case sg.WIN_CLOSED:
+            break
 window.close()
